49e6fc2307b69c6670af7797e8ea4dd15ef8ba1d
Xiaoming <huxiaoluder@163.com>
2018-12-26 10:07:39 +0800

    do some configture

--- source/MediaStreamPlayer.h
+++ source/MediaStreamPlayer.h
@@ -9,6 +9,20 @@
 #ifndef MediaStreamPlayer_h
 #define MediaStreamPlayer_h
 
+#include "MSCodecProtocol.h"
+#include "MSMediaData.hpp"
+#include "MSTimer.hpp"
+#include "MSPlayer.hpp"
 
+// FFmpeg 跨平台软解/软编
+#include "FFCodecContext.hpp"
+#include "FFDecoder.hpp"
+#include "FFEncoder.hpp"
+
+#ifdef __APPLE__
+// ios 硬解/硬编
+#elif
+// android
+#endif
 
 #endif /* MediaStreamPlayer_h */
